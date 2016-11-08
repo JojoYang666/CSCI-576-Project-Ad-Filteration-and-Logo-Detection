704013445a83b6afa2edcc1c199fdc4c8de6cd44
Akshay Atul Deshmukh <akshayatuldeshmukh31@gmail.com>
2016-11-08 15:04:45 -0800

    Shot.java completed

---
+++ src/com/project/Shot.java
@@ -0,0 +1,50 @@
+package com.project;
+
+class Shot{
+	private double yMean;
+	private double ampMean;
+	private long startingByte;
+	private long lengthOfShot;
+	
+	public Shot(double yMean, double ampMean, long startingByte, long lengthOfShot) {
+		super();
+		this.yMean = yMean;
+		this.ampMean = ampMean;
+		this.startingByte = startingByte;
+		this.lengthOfShot = lengthOfShot;
+	}
+
+
+	public double getyMean() {
+		return yMean;
+	}
+
+	public void setyMean(double yMean) {
+		this.yMean = yMean;
+	}
+
+	public double getAmpMean() {
+		return ampMean;
+	}
+
+	public void setAmpMean(double ampMean) {
+		this.ampMean = ampMean;
+	}
+
+	public long getStartingByte() {
+		return startingByte;
+	}
+
+	public void setStartingByte(long startingByte) {
+		this.startingByte = startingByte;
+	}
+
+	public long getLengthOfShot() {
+		return lengthOfShot;
+	}
+
+	public void setLengthOfShot(long lengthOfShot) {
+		this.lengthOfShot = lengthOfShot;
+	}
+	
+}
