9886f57613e80667d12615d9e659b8552c585baa
Youngmin <shin630@usc.edu>
2016-11-11 14:31:48 -0800

    Initiate playing video. However, there is a sync problem.

---
+++ src/MyPlayer.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by Youngmin on 2016. 11. 10..
+ */
+public class MyPlayer {
+    public static void main(String[] args) {
+
+        String videoFilename = "data_test1.rgb";
+        String audioFilename = "data_test1.wav";
+
+        // opens the inputStream
+        FileInputStream inputStream;
+        try {
+            inputStream = new FileInputStream(audioFilename);
+            // inputStream = this.getClass().getResourceAsStream(filename);
+
+            // initializes the playSound Object
+            PlaySound playSound = new PlaySound(inputStream);
+            PlayVideo playVideo = new PlayVideo(videoFilename, playSound);
+
+            Thread t1 = new Thread(playSound);
+            Thread t2 = new Thread(playVideo);
+
+            t1.start();
+            t2.start();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+}
