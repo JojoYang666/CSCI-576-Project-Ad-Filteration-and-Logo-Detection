841af06b5ee6d8bf49b2b0faf5eb569403a24844
Akshay Atul Deshmukh <akshayatuldeshmukh31@gmail.com>
2016-11-15 17:29:32 -0800

    MakeShots() completed successfully. Time to make shots for 3.5GB - 3:20 minutes

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -1,23 +1,20 @@
-
-
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-public class MyProgram{
+public class MyProgram {
 	private static ArrayList<Shot> shots;
-	private double globalYMean;
-	private double globalMseAmp;
 	private static final int width = 480, height = 270;
 	private ArrayList<Scene> scenes;
+	private static ArrayList<Double> valsY = new ArrayList<Double>();
+	private static ArrayList<Double> valsU = new ArrayList<Double>();
+	private static ArrayList<Double> valsV = new ArrayList<Double>();
 	private static String inputVideoFile, inputAudioFile, outputVideoFile, outputAudioFile;
-	private static double currentShotLumTotal;
+	private static int sizeOfSlidingWindow = 5;
 	private static double[][] currentYMatrix = new double[width][height];
 	private static double[][] prevYMatrix = new double[width][height];
 	private static double[][] currentUMatrix = new double[width][height];
@@ -26,97 +23,109 @@ public class MyProgram{
 	private static double[][] prevVMatrix = new double[width][height];
 	private static YUV currentYUV;
 
-	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		parseArgs(args);
 		FrameReader fReader = new FrameReader(inputVideoFile, width, height);
 		makeShots(fReader);
 		System.out.println("SIZE OF FILE - " + fReader.getFileLength());
 		System.out.println("SIZE OF SHOTS - " + shots.size());
-		for(Shot s: shots){
+		for (Shot s : shots) {
 			System.out.println("SHOT: " + s.getStartingByte() + "   " + s.getLengthOfShot());
 		}
-		//for(Shot s: shots){
-		//	System.out.println(s.getStartingByte() + " " + s.getLengthOfShot());
-		//}
 		writeToDisk();
-		//makeScenes();
+		// makeScenes();
 		fReader.close();
 	}
+
 	/**
 	 * Making Shot object using the frames from Frame Reader
+	 * 
 	 * @param fReader
 	 */
-	private static void makeShots(FrameReader fReader){
-		long maxNumOfFrames = fReader.getNumberOfFrames();
-		long offset = 0, numOfFrames = 0;
-		double yFrameAvg = 0;
+	private static void makeShots(FrameReader fReader) {
+		long maxNumOfFrames = fReader.getNumberOfFrames(), offset = 0;
 		double interFrameDiffEstimateY = 0, interFrameDiffEstimateU = 0, interFrameDiffEstimateV = 0;
-		double shotBasedDiffEstimateY = 0, shotBasedDiffEstimateU = 0, shotBasedDiffEstimateV = 0;
-		boolean newShot = true, firstFrameDiffEstimate = true;					
+		double yThreshold = 0, uThreshold = 0, vThreshold = 0;
+		int votes = 0;
+		boolean startProcess = true, firstFrameDiffEstimate = true;
 		shots = new ArrayList<Shot>();
-		
+
 		System.out.println("MAX NUMBER OF FRAMES - " + maxNumOfFrames);
 
-		//TODO Audio processing
-		while(offset<maxNumOfFrames){
-			//System.out.println("CURRENT FRAME - " + offset);
-			if(newShot==false){
-                currentYUV = fReader.read();
-				currentYMatrix = currentYUV.getY();
-				currentUMatrix = currentYUV.getU();
-				currentVMatrix = currentYUV.getV();
-				yFrameAvg = getFrameAvg();
-				numOfFrames++;
-				interFrameDiffEstimateY = getFrameDifference(currentYMatrix, prevYMatrix);
-				interFrameDiffEstimateU = getFrameDifference(currentUMatrix, prevUMatrix);
-				interFrameDiffEstimateV = getFrameDifference(currentVMatrix, prevVMatrix);
-				
-				if(firstFrameDiffEstimate==false){
-					if(((interFrameDiffEstimateY*0.1 + interFrameDiffEstimateU*0.45 + interFrameDiffEstimateV*0.45) - (shotBasedDiffEstimateY/(numOfFrames-2)*0.1 + shotBasedDiffEstimateU/(numOfFrames-2)*0.45 + shotBasedDiffEstimateV/(numOfFrames-2)*0.45))>(0.2*(shotBasedDiffEstimateY/(numOfFrames-2)*0.1 + shotBasedDiffEstimateU/(numOfFrames-2)*0.45 + shotBasedDiffEstimateV/(numOfFrames-2)*0.45))){
-						//Exceeded threshold
-						newShot = true;
-						firstFrameDiffEstimate = true;
-						shots.get(shots.size()-1).setLengthOfShot((offset*fReader.getLen()) - shots.get(shots.size()-1).getStartingByte());
-						shots.get(shots.size()-1).setyMean(currentShotLumTotal/(numOfFrames-1));
-						currentShotLumTotal = 0;
-						shotBasedDiffEstimateY = 0;
-						shotBasedDiffEstimateU = 0;
-						shotBasedDiffEstimateV = 0;
-						numOfFrames = 0;
-						//System.out.println(shots.get(shots.size()-1).getyMean());
-					}
-					else{
-						currentShotLumTotal += yFrameAvg;
-						offset+=1;
-					}
-				}
-				else if(firstFrameDiffEstimate==true){
-					currentShotLumTotal += yFrameAvg;
-					offset += 1;
-					firstFrameDiffEstimate = false;
-				}
-				shotBasedDiffEstimateY += interFrameDiffEstimateY;
-				shotBasedDiffEstimateU += interFrameDiffEstimateU;
-				shotBasedDiffEstimateV += interFrameDiffEstimateV;
-			}
-			
-			if(newShot==true){
-				shots.add(new Shot());
-				shots.get(shots.size()-1).setStartingByte(offset*fReader.getLen());
+		// TODO Audio processing
+		while (offset < maxNumOfFrames) {
+			System.out.println("CURRENT FRAME - " + offset );
+			if (startProcess == false) {
 				currentYUV = fReader.read();
 				currentYMatrix = currentYUV.getY();
 				currentUMatrix = currentYUV.getU();
 				currentVMatrix = currentYUV.getV();
-				yFrameAvg = getFrameAvg();
-				numOfFrames++;
-				currentShotLumTotal = yFrameAvg;
-				offset+=1;
-				newShot = false;
+				interFrameDiffEstimateY = blockMatchingAlgo(currentYMatrix, prevYMatrix);
+				interFrameDiffEstimateU = blockMatchingAlgo(currentUMatrix, prevUMatrix);
+				interFrameDiffEstimateV = blockMatchingAlgo(currentVMatrix, prevVMatrix);
+
+				if(valsY.size()==sizeOfSlidingWindow)
+					valsY.remove(0);
+				if(valsU.size()==sizeOfSlidingWindow)
+					valsU.remove(0);
+				if(valsV.size()==sizeOfSlidingWindow)
+					valsV.remove(0);				
+				
+				if(firstFrameDiffEstimate==true){
+					firstFrameDiffEstimate = false;
+					valsY.add((double) interFrameDiffEstimateY);
+					valsU.add((double) interFrameDiffEstimateU);
+					valsV.add((double) interFrameDiffEstimateV);
+				}
+				else{
+					votes = 0;
+					if(interFrameDiffEstimateY>yThreshold)
+						votes++;
+					if(interFrameDiffEstimateU>uThreshold)
+						votes++;
+					if(interFrameDiffEstimateV>vThreshold)
+						votes++;
+					if(votes>=2){
+//						System.out.println("THAT'S A SHOT, BOYS! " + offset);
+						shots.get(shots.size() - 1).setLengthOfShot((offset * fReader.getLen()) - shots.get(shots.size() - 1).getStartingByte());
+//						System.out.println(shots.get(shots.size()-1).getStartingByte() + " " + offset*fReader.getLen() + " " + shots.get(shots.size()-1).getLengthOfShot());
+						shots.add(new Shot());
+						shots.get(shots.size()-1).setStartingByte(offset*fReader.getLen());
+						firstFrameDiffEstimate = true;
+						valsY.clear();
+						valsU.clear();
+						valsV.clear();
+					}
+					else{
+						valsY.add((double) interFrameDiffEstimateY);
+						valsU.add((double) interFrameDiffEstimateU);
+						valsV.add((double) interFrameDiffEstimateV);
+					}
+				}
+				
+				yThreshold = 2*calcThreshold(valsY);
+				uThreshold = 2*calcThreshold(valsU);
+				vThreshold = 2*calcThreshold(valsV);
+				offset += 1;
 			}
 
-			for(int x = 0; x<width; x++){
-				for(int y = 0; y<height; y++){
+			if (startProcess == true) {
+				System.out.println("Starting with " + offset);
+				shots.add(new Shot());
+				shots.get(shots.size() - 1).setStartingByte(offset * fReader.getLen());
+				currentYUV = fReader.read();
+				currentYMatrix = currentYUV.getY();
+				currentUMatrix = currentYUV.getU();
+				currentVMatrix = currentYUV.getV();
+				valsY.clear();
+				valsU.clear();
+				valsV.clear();
+				offset += 1;
+				startProcess = false;
+			}
+
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
 					prevYMatrix[x][y] = currentYMatrix[x][y];
 					prevUMatrix[x][y] = currentUMatrix[x][y];
 					prevVMatrix[x][y] = currentVMatrix[x][y];
@@ -124,14 +133,25 @@ public class MyProgram{
 			}
 		}
 
-		if(numOfFrames!=0){
-			shots.get(shots.size()-1).setLengthOfShot(fReader.getFileLength() - shots.get(shots.size()-1).getStartingByte());
-			shots.get(shots.size()-1).setyMean(currentShotLumTotal/(numOfFrames-1));
+		shots.get(shots.size() - 1).setLengthOfShot(fReader.getFileLength() - shots.get(shots.size() - 1).getStartingByte());
+	}
+
+	private static double calcThreshold(ArrayList<Double> vals){
+		double avg = 0, sumOfDiff = 0;
+		for(int i = 0; i<vals.size();i++){
+			avg += vals.get(i);
 		}
+		avg /= vals.size();
+		
+		for(int i = 0; i<vals.size(); i++){
+			sumOfDiff += Math.abs(vals.get(i) - avg);
+		}
+		sumOfDiff /= vals.size();
+		return (avg + sumOfDiff);
 	}
 	
-	private static void writeToDisk(){
-        final int MAX_LIMIT = 999999999;
+	private static void writeToDisk() {
+		final int MAX_LIMIT = 999999999;
 		String fileName = "output";
 		int fileNo = 1;
 		String extension = ".rgb";
@@ -143,67 +163,100 @@ public class MyProgram{
 		} catch (FileNotFoundException e) {
 			Utilities.die("File not found - " + inputVideoFile);
 		}
-		
-		for(Shot s: shots){
-			try  
-			{
+
+		for (Shot s : shots) {
+			try {
 				FileOutputStream fw = new FileOutputStream(new File(fileName + fileNo + extension));
-                long bytesRemaining = s.getLengthOfShot();
-                byte[] bytes;
-                while (bytesRemaining != 0){
-                    if (bytesRemaining >= MAX_LIMIT){
-                        bytes = new byte[MAX_LIMIT];
-                    }
-                    else{
-                        bytes = new byte[(int)bytesRemaining];
-                    }
-                    inputFileStream.read(bytes);
-                    fw.write(bytes);
-                    bytesRemaining -= bytes.length;
-                }
-			    fw.close();   
-			    fileNo++;
-			}
-			catch (IOException e)
-			{
-			    System.err.println("Error: " + e.getMessage());
+				long bytesRemaining = s.getLengthOfShot();
+				byte[] bytes;
+				while (bytesRemaining != 0) {
+					if (bytesRemaining >= MAX_LIMIT) {
+						bytes = new byte[MAX_LIMIT];
+					} else {
+						bytes = new byte[(int) bytesRemaining];
+					}
+					inputFileStream.read(bytes);
+					fw.write(bytes);
+					bytesRemaining -= bytes.length;
+				}
+				fw.close();
+				fileNo++;
+			} catch (IOException e) {
+				System.err.println("Error: " + e.getMessage());
 			}
 		}
-	}
-	
-	private static void makeScenes(){
-		
-	}
-	
-	private static double getFrameAvg(){
-		double frameAvg = 0;
-		for(int y = 0;y<width;y++){
-			for(int x = 0; x<height; x++){
-				frameAvg += currentYMatrix[y][x]; 
-			}
-		}
-		return frameAvg/(width*height);
 	}
 
-	private static double getFrameDifference(double[][] currentFrameLum, double[][] prevFrameLum){
-		double frameDiff = 0;
-		for(int x = 0; x<width; x++){
-			for(int y = 0; y<height; y++){
-				frameDiff += Math.pow(currentFrameLum[x][y] - prevFrameLum[x][y],2);
+	private static void makeScenes() {
+
+	}
+
+	private static double getFrameAvg() {
+		double frameAvg = 0;
+		for (int y = 0; y < width; y++) {
+			for (int x = 0; x < height; x++) {
+				frameAvg += currentYMatrix[y][x];
 			}
 		}
-		return frameDiff/(width*height);
+		return frameAvg / (width * height);
 	}
-	
-	private static void parseArgs(String[] args){
-		if(args.length!=1){
-			Utilities.die("Not enough arguments! \nUsage java MyProgram input.rgb input.wav output.rgb output.wav");		
-		}
+
+	private static double blockMatchingAlgo(double[][] currentFrame, double[][] prevFrame){
+		double frameDiff = 0;
+		double blockDiff = 0;
+		double macroblockDiff = 0;
+		int stepSize, posX, posY, startX, startY;
 		
-		inputVideoFile = args[0];
-		//inputAudioFile = args[1];
-		//outputVideoFile = args[2];
-		//outputAudioFile = args[3];
+		for(int x = 0; x<(width-16); x+=16){
+			for(int y = 0; y<(height-16); y+=16){
+				macroblockDiff = Double.MAX_VALUE;
+				stepSize = 4;
+				startX = x;
+				startY = y;
+				posX = x;
+				posY = y;
+				
+				while(stepSize>0){
+					startX = posX;
+					startY = posY;
+					for(int regionX = -stepSize; regionX<=stepSize; regionX+=stepSize){
+						if((startX+regionX < 0)|(startX+regionX >= width)|(startX+regionX+16 >= width))
+							continue;
+						for(int regionY = -stepSize; regionY<=stepSize; regionY+=stepSize){
+							if((startY+regionY < 0)|(startY+regionY >= height)|(startY+regionY+16 >= height))
+								continue;
+							if(regionX==0 && regionY==0)
+								continue;
+							blockDiff = 0;
+							for(int iDisp = 0; iDisp<16; iDisp++){
+								for(int jDisp = 0; jDisp<16; jDisp++){
+									blockDiff += Math.abs(currentFrame[startX+iDisp][startY+jDisp] - prevFrame[startX+regionX+iDisp][startY+regionY+jDisp]);
+								}
+							}
+							if(macroblockDiff > blockDiff/256){
+								posX = startX + regionX;
+								posY = startY + regionY;
+								macroblockDiff = blockDiff/256;
+							}
+						}
+					}
+					stepSize /= 2;
+				}
+				frameDiff += macroblockDiff;
+			}
+		}
+		return frameDiff;
 	}
-	
+
+	private static void parseArgs(String[] args) {
+		if (args.length != 1) {
+			Utilities.die("Not enough arguments! \nUsage java MyProgram input.rgb input.wav output.rgb output.wav");
+		}
+
+		inputVideoFile = args[0];
+		// inputAudioFile = args[1];
+		// outputVideoFile = args[2];
+		// outputAudioFile = args[3];
+	}
+
 }
