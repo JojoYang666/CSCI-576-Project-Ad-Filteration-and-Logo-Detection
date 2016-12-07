a0e825e9374514ed5f51d98ff3d918671a8ab8ac
Akshay Atul Deshmukh <akshayatuldeshmukh31@gmail.com>
2016-12-06 17:56:37 -0800

    Linking logo detectors

--- src/VideoFrame.java
+++ src/VideoFrame.java
@@ -1,30 +1,53 @@
-import java.util.ArrayList;
-
 public class VideoFrame {
-    private int frameNumber;
-    private int[] logoLocations;
-  
-    public VideoFrame() {}
-  
-    public VideoFrame(int frameNum) {
-        this.frameNumber = frameNum;
-        this.logoLocations = new int[] {-1, -1, -1, -1};
-    }
-  
-    public int getFrameNumber() {
-        return(this.frameNumber);
-    }
-  
-    public void setFrameNumber(int num) {
-        this.frameNumber = num;
-    }
-  
-    public int[] getLogoLocations() {
-        return(this.logoLocations);
-    }
-  
-    // logo: 0 = starbucks, 1 = subway, 2 = mcd, 3 = nfl
-    public void addLogoLocation(int logo, int location) {
-        this.logoLocations[logo] = location;
-    }
+	private int frameNumber;
+	private int logoLocation;
+	private float confidence;
+	private int logo;
+
+	// logo: 0 = starbucks, 1 = subway, 2 = mcd, 3 = nfl
+	public VideoFrame() {
+		confidence = -1;
+		logoLocation = -1;
+		logo = -1;
+	}
+
+	public VideoFrame(int frameNumber, int logoLocation, float confidence, int logo) {
+		super();
+		this.frameNumber = frameNumber;
+		this.logoLocation = logoLocation;
+		this.confidence = confidence;
+		this.logo = logo;
+	}
+
+	public int getFrameNumber() {
+		return frameNumber;
+	}
+
+	public void setFrameNumber(int frameNumber) {
+		this.frameNumber = frameNumber;
+	}
+
+	public int getLogoLocation() {
+		return logoLocation;
+	}
+
+	public void setLogoLocation(int logoLocation) {
+		this.logoLocation = logoLocation;
+	}
+
+	public float getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(float confidence) {
+		this.confidence = confidence;
+	}
+
+	public int getLogo() {
+		return logo;
+	}
+
+	public void setLogo(int logo) {
+		this.logo = logo;
+	}
 }
